88ff808ae4b8cc193ca367f6467bbd2beb109c9b
nguyentatcongtnut-crypto <nguyentatcongtnut@gmail.com>
2025-12-27 23:15:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        Base: '/-CNC-ASSY-PQC-P-RMA-And-Sorting-Report/' 
       server: {
         port: 3000,
         host: '0.0.0.0',
